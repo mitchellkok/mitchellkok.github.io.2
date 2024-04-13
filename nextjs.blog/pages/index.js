bd7d67a9621e278684469d3d30d1b84335c52250
Mitchell Kok <mitchellkokme@gmail.com>
2024-04-13 16:12:13 +0800

    Added link to first post

--- nextjs.blog/pages/index.js
+++ nextjs.blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <Link href="/posts/first-post.js">our Next.js tutorial</Link>.)
         </p>
       </section>
     </Layout>
