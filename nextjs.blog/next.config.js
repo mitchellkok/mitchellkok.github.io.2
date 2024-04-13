3a221f475773b8a801ec89c704a151efd68f1faa
Mitchell Kok <mitchellkokme@gmail.com>
2024-04-13 15:56:19 +0800

    Updated next config

---
+++ nextjs.blog/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
